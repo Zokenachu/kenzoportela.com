d970406a5fbdacba2320480551e00e8a64afbf06
Zokenachu <89014729+Zokenachu@users.noreply.github.com>
2024-07-30 15:06:21 +0200

    Delete beercss directory

--- beercss/index.js
+++
@@ -1,9 +0,0 @@
-import "src/cdn/beer.css";
-import "src/cdn/material-symbols-outlined.woff2";
-import "src/cdn/roboto-flex-cyrillic-ext.woff2";
-import "src/cdn/roboto-flex-cyrillic.woff2";
-import "src/cdn/roboto-flex-greek.woff2";
-import "src/cdn/roboto-flex-latin-ext.woff2";
-import "src/cdn/roboto-flex-latin.woff2";
-import "src/cdn/roboto-flex-vietnamese.woff2";
-import beercss from "src/cdn/beer.ts";
