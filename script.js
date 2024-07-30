a02e6fc9b08e728a6c36cd7f53cbd6d557590039
Zokenachu <89014729+Zokenachu@users.noreply.github.com>
2024-07-30 14:57:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnMode =  document.querySelector('#btnMode');
+
+btnMode.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+
+    }else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+
+    }
+
+})
